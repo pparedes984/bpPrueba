50c5855d3280e65cd4316d6a204ff5b1ec5cc81e
pparedes <pparedes@cobiscorp.com>
2025-02-10 22:59:09 -0500

    PAP - Se añaden funcionalidades en frontend

--- bp-frontend-microservice/src/app/features/cuentas/cuentas.component.ts
+++ bp-frontend-microservice/src/app/features/cuentas/cuentas.component.ts
@@ -2,51 +2,100 @@ import { Component, OnInit } from '@angular/core';
 import { Account } from '../../core/models/account.module';
 import { CuentasService } from '../../core/services/cuentas.service';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-cuentas',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   standalone: true,
   templateUrl: './cuentas.component.html',
   styleUrl: './cuentas.component.scss'
 })
 export class CuentasComponent implements OnInit{
   accounts: Account[] = [];
-  accountUpdated: Account | null = null;
+  accountsFiltradas: Account[] = [];
+  formularioVisible = false;
+  editando = false;
+  filtroId = '';
+  accountForm: FormGroup;
 
-  constructor(private accountService: CuentasService) {}
+  constructor(private accountService: CuentasService, private fb: FormBuilder) {
+    this.accountForm = this.fb.group({
+          id: [null],
+          accountNumber: ['', Validators.required],
+          accountType: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+          openingBalance: ['', Validators.required],
+          state: ['', Validators.required],
+          clientId: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
     this.cargarAccounts();
+    this.accountForm.get('id')?.valueChanges
+          .pipe(debounceTime(300))
+          .subscribe(value => this.filtrarAccounts(value));
   }
 
-  cargarAccounts() {
-    this.accountService.getAccounts().subscribe(data => {
-      console.log(data);
-      this.accounts = data;
+  cargarAccounts():void {
+    this.accountService.getAccounts().subscribe({
+      next: accounts => {
+        this.accounts = accounts;
+        this.accountsFiltradas = accounts;
+      },
+      error: err => console.error('Error al cargar cuentas:', err)
     });
+  }
+
+  filtrarAccounts(termino: string): void {
+    const idBuscado = Number(termino)
+    this.accountsFiltradas = this.accounts.filter(account =>
+      account.id === idBuscado
+    );
+  }
+
+  nuevaAccount(): void {
+    this.accountForm.reset();
+    this.formularioVisible = true;
+    this.editando = false;
+  }
+
+  updateAccount(account: Account): void {
+    this.accountForm.patchValue(account);
+    this.formularioVisible = true;
+    this.editando = true;
   }
 
   deleteAccount(id: number) {
     if (confirm('¿Estás seguro de eliminar esta cuenta?')) {
-      this.accountService.deleteAccount(id).subscribe(() => {
-        this.accounts = this.accounts.filter(account => account.id !== id);
+      this.accountService.deleteAccount(id).subscribe({
+        next: () => this.cargarAccounts(),
+        error: err => console.error('Error al eliminar cuenta:', err)
       });
     }
   }
 
-  updateAccount(account: Account) {
-    this.accountUpdated = { ...account };
-  }
-
-  guardarEdicion() {
-    if (this.accountUpdated) {
-      this.accountService.updateAccount(this.accountUpdated.id, this.accountUpdated)
-        .subscribe(() => {
-          this.cargarAccounts();
-          this.accountUpdated = null;
-        });
+  guardarAccount(): void {
+    if (this.accountForm.invalid) return;
+    const account = this.accountForm.value;
+    if (this.editando) {
+      this.accountService.updateAccount(account.id, account).subscribe({
+        next: () => this.cargarAccounts(),
+        error: err => console.error('Error al actualizar cuenta:', err)
+      });
+    } else {
+      this.accountService.createAccount(account).subscribe({
+        next: () => this.cargarAccounts(),
+        error: err => console.error('Error al crear cuenta:', err)
+      });
     }
+    this.formularioVisible = false;
   }
-
+  // Método para cancelar el formulario
+  cancelarFormulario(): void {
+    this.formularioVisible = false; // Ocultar el formulario
+    this.accountForm.reset(); // Opcional: resetear el formulario
+  }
 }
+
